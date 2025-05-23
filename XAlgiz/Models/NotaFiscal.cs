9745fffd76d09b34669f16f115aedacfdb075e33
AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>
2025-05-23 04:56:45 -0300

    add class NotaFiscal
    Signed-off-by: AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>

---
+++ XAlgiz/Models/NotaFiscal.cs
@@ -0,0 +1,22 @@
+﻿namespace XAlgiz.Models;
+
+public class NotaFiscal
+{
+    public NotaFiscal()
+    {
+    }
+
+    public NotaFiscal(int numero, DateOnly data, int clienteId, int servicoId)
+    {
+        Numero = numero;
+        Data = data;
+        ClienteId = clienteId;
+        ServicoId = servicoId;
+    }
+
+    public int Id { get; set; }
+    public int Numero { get; set; }
+    public DateOnly Data { get; set; }
+    public int ClienteId { get; set; }
+    public int ServicoId { get; set; }
+}
