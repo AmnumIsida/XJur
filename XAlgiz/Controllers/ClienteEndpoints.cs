28859a827ffc10db99e9f40ecb8b3d8459ea18d4
AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>
2025-05-23 05:18:26 -0300

    add class ClienteEndpoints
    Signed-off-by: AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>

---
+++ XAlgiz/Controllers/ClienteEndpoints.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using XAlgiz.Dtos;
+using XAlgiz.Services;
+
+namespace XAlgiz.Controllers;
+
+public static class ClienteEndpoints
+{
+    public static void MapClienteEndpoints(this WebApplication app)
+    {
+        app.MapPost("/clientes",
+                (CadastrarClienteRequest request, ClienteService service,
+                    IValidator<CadastrarClienteRequest> validator) =>
+                {
+                    var validation = validator.Validate(request);
+                    if (!validation.IsValid)
+                        return Results.BadRequest(validation.Errors.Select(e => e.ErrorMessage));
+
+                    try
+                    {
+                        return Results.Ok(service.CadastrarCliente(request));
+                    }
+                    catch
+                    {
+                        return Results.StatusCode(500);
+                    }
+                })
+            .RequireAuthorization()
+            .WithName("CadastrarCliente")
+            .WithTags("Clientes")
+            .Produces<CadastrarClienteResponse>()
+            .Produces<IEnumerable<string>>(StatusCodes.Status400BadRequest)
+            .Produces(StatusCodes.Status500InternalServerError)
+            .WithOpenApi();
+    }
+}
