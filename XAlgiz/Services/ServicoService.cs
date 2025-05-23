1f3b8a4799d5422795b58ffdb650a7d84e287505
AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>
2025-05-23 05:14:01 -0300

    add class ServicoService
    Signed-off-by: AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>

---
+++ XAlgiz/Services/ServicoService.cs
@@ -0,0 +1,28 @@
+﻿using XAlgiz.Data;
+using XAlgiz.Dtos;
+using XAlgiz.Exceptions;
+using XAlgiz.Models;
+
+namespace XAlgiz.Services;
+
+public class ServicoService(AppDbContext context) : BaseService(context)
+{
+    public CadastrarServicoResponse CadastrarServico(CadastrarServicoRequest requisicao)
+    {
+        var cliente = Context.Clientes.Find(requisicao.ClientId);
+        if (cliente == null) throw new ClienteNaoEncontradoException();
+
+        var servico = new Servico(cliente.Id, requisicao.Descricao, requisicao.Valor, requisicao.Data);
+
+        Context.Servicos.Add(servico);
+        Context.SaveChanges();
+
+        return new CadastrarServicoResponse(
+            servico.Id,
+            servico.ClienteId,
+            servico.Descricao,
+            servico.Valor,
+            servico.Data
+        );
+    }
+}
