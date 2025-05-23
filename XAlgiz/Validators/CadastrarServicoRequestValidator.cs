704a9feb7997783ed26243cb586e3bbc40724179
AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>
2025-05-23 05:18:12 -0300

    add class CadastrarServicoRequestValidator
    Signed-off-by: AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>

---
+++ XAlgiz/Validators/CadastrarServicoRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using XAlgiz.Dtos;
+
+namespace XAlgiz.Validators;
+
+public class CadastrarServicoRequestValidator : AbstractValidator<CadastrarServicoRequest>
+{
+    public CadastrarServicoRequestValidator()
+    {
+        RuleFor(r => r.ClientId)
+            .GreaterThan(0).WithMessage("Id do cliente inválido.");
+
+        RuleFor(r => r.Descricao)
+            .NotEmpty().WithMessage("Descrição é obrigatória.");
+
+        RuleFor(r => r.Valor)
+            .GreaterThan(0).WithMessage("Valor menor que zero.");
+    }
+}
