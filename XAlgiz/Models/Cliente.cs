8f10d2197d13f0a7810f86096d9c9144e7365814
AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>
2025-05-23 04:55:18 -0300

    add class Cliente
    Signed-off-by: AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>

---
+++ XAlgiz/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿namespace XAlgiz.Models;
+
+public class Cliente
+{
+    public Cliente(string nome, string cpfCnpj, string email, string telefone)
+    {
+        Nome = nome;
+        CpfCnpj = cpfCnpj;
+        Email = email;
+        Telefone = telefone;
+    }
+
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string CpfCnpj { get; set; }
+    public string Email { get; set; }
+    public string Telefone { get; set; }
+}
