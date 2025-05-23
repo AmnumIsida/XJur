25bc0da225509178bf9d92b2b4170169ddca9ee0
AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>
2025-05-23 04:56:10 -0300

    add class Servico
    Signed-off-by: AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>

---
+++ XAlgiz/Models/Servico.cs
@@ -0,0 +1,22 @@
+﻿namespace XAlgiz.Models;
+
+public class Servico
+{
+    public Servico()
+    {
+    }
+
+    public Servico(int clienteId, string descricao, decimal valor, DateOnly data)
+    {
+        ClienteId = clienteId;
+        Descricao = descricao;
+        Valor = valor;
+        Data = data;
+    }
+
+    public int Id { get; set; }
+    public string Descricao { get; set; }
+    public decimal Valor { get; set; }
+    public DateOnly Data { get; set; }
+    public int ClienteId { get; set; }
+}
