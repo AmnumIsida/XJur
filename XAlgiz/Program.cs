a88bc297517d786fb9d53c91c45b2969f3b980e0
AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>
2025-05-23 05:19:01 -0300

    edit Program.cs
    Signed-off-by: AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>

--- XAlgiz/Program.cs
+++ XAlgiz/Program.cs
@@ -1,13 +1,51 @@
+using FluentValidation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using XAlgiz.Controllers;
+using XAlgiz.Data;
+using XAlgiz.Dtos;
+using XAlgiz.Security;
+using XAlgiz.Services;
+using XAlgiz.Validators;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("DBTeste"));
+
+builder.Services.AddScoped<ClienteService>();
+builder.Services.AddScoped<ServicoService>();
+builder.Services.AddScoped<NotaFiscalService>();
+
+builder.Services.AddTransient<IValidator<CadastrarClienteRequest>, CadastrarClienteRequestValidator>();
+builder.Services.AddTransient<IValidator<CadastrarServicoRequest>, CadastrarServicoRequestValidator>();
+builder.Services.AddTransient<IValidator<EmitirNotaFiscalRequest>, EmitirNotaFiscalRequestValidator>();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false, // Configure conforme seu cenário
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(JwtSettings.Key)
+        };
+    });
+
+builder.Services.AddAuthorization();
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -16,29 +54,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthentication();
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+app.MapSecurityEndpoints();
+app.MapClienteEndpoints();
+app.MapServicoEndpoints();
+app.MapNotaFiscalEndpoints();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
