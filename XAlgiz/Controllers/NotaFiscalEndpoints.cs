3a045a7d7be91459252ab16deaa9c18f1de74e1f
AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>
2025-05-23 05:18:33 -0300

    add class NotaFiscalEndpoints
    Signed-off-by: AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>

---
+++ XAlgiz/Controllers/NotaFiscalEndpoints.cs
@@ -0,0 +1,47 @@
+﻿using FluentValidation;
+using XAlgiz.Dtos;
+using XAlgiz.Exceptions;
+using XAlgiz.Services;
+
+namespace XAlgiz.Controllers;
+
+public static class NotaFiscalEndpoints
+{
+    public static void MapNotaFiscalEndpoints(this WebApplication app)
+    {
+        app.MapPost("/notas-fiscais",
+                (EmitirNotaFiscalRequest request, NotaFiscalService service,
+                    IValidator<EmitirNotaFiscalRequest> validator) =>
+                {
+                    var validation = validator.Validate(request);
+                    if (!validation.IsValid)
+                        return Results.BadRequest(validation.Errors.Select(e => e.ErrorMessage));
+
+                    try
+                    {
+                        return Results.Ok(service.EmitirNotaFiscal(request));
+                    }
+                    catch (ServicoJaPossuiNotaFiscalException)
+                    {
+                        return Results.BadRequest(new[] { "Serviço já possui nota fiscal associada." });
+                    }
+                    catch (ServicoNaoEncontradoException)
+                    {
+                        return Results.BadRequest(new[] { "Serviço não encontrado." });
+                    }
+                    catch (ClienteNaoEncontradoException)
+                    {
+                        return Results.BadRequest(new[] { "Cliente não encontrado." });
+                    }
+                    catch (Exception e)
+                    {
+                        return Results.Problem(e.Message);
+                    }
+                }).RequireAuthorization()
+            .WithName("EmitirNotaFiscal")
+            .WithTags("Notas Fiscais")
+            .Produces<EmitirNotaFiscalResponse>()
+            .Produces<IEnumerable<string>>(StatusCodes.Status400BadRequest)
+            .Produces(StatusCodes.Status500InternalServerError);
+    }
+}
