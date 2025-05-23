739dce033742688cebf106af049d69acaf2f146d
AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>
2025-05-23 04:58:36 -0300

    add class AppDbContext
    Signed-off-by: AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>

---
+++ XAlgiz/Data/AppDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using XAlgiz.Models;
+
+namespace XAlgiz.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Cliente> Clientes { get; set; }
+    public DbSet<Servico> Servicos { get; set; }
+    public DbSet<NotaFiscal> NotasFiscais { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Definição das chaves primárias
+        modelBuilder.Entity<Cliente>().HasKey(c => c.Id);
+        modelBuilder.Entity<Servico>().HasKey(s => s.Id);
+        modelBuilder.Entity<NotaFiscal>().HasKey(n => n.Id);
+
+        // Definição dos campos obrigatórios
+        modelBuilder.Entity<Cliente>()
+            .Property(c => c.Nome)
+            .IsRequired();
+
+        modelBuilder.Entity<Cliente>()
+            .Property(c => c.CpfCnpj)
+            .IsRequired();
+
+        modelBuilder.Entity<Cliente>()
+            .Property(c => c.Email)
+            .IsRequired();
+
+        modelBuilder.Entity<Cliente>()
+            .Property(c => c.Telefone)
+            .IsRequired();
+
+        modelBuilder.Entity<Servico>()
+            .Property(s => s.Descricao)
+            .IsRequired();
+
+        modelBuilder.Entity<Servico>()
+            .Property(s => s.Valor)
+            .IsRequired();
+    }
+}
