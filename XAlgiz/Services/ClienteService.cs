39d74f71a1d74ef7eb37f4d80d0aedc52f576c0c
AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>
2025-05-23 05:13:43 -0300

    add class ClienteService
    Signed-off-by: AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>

---
+++ XAlgiz/Services/ClienteService.cs
@@ -0,0 +1,26 @@
+﻿using XAlgiz.Data;
+using XAlgiz.Dtos;
+using XAlgiz.Models;
+
+namespace XAlgiz.Services;
+
+public class ClienteService(AppDbContext context) : BaseService(context)
+{
+    public CadastrarClienteResponse CadastrarCliente(CadastrarClienteRequest requisicao)
+    {
+        var cliente = new Cliente(
+            requisicao.Nome,
+            requisicao.CpfCnpj,
+            requisicao.Email,
+            requisicao.Telefone
+        );
+        Context.Clientes.Add(cliente);
+        Context.SaveChanges();
+        return new CadastrarClienteResponse(
+            cliente.Id,
+            cliente.Nome,
+            cliente.CpfCnpj,
+            cliente.Email,
+            cliente.Telefone);
+    }
+}
