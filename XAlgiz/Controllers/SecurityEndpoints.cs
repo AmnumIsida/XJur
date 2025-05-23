9acdd15de1e6205c12126544f43e5d852323e9b1
AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>
2025-05-23 05:18:40 -0300

    add class SecurityEndpoints
    Signed-off-by: AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>

---
+++ XAlgiz/Controllers/SecurityEndpoints.cs
@@ -0,0 +1,33 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using XAlgiz.Dtos;
+using XAlgiz.Security;
+
+namespace XAlgiz.Controllers;
+
+public static class SecurityEndpoints
+{
+    public static void MapSecurityEndpoints(this WebApplication app)
+    {
+        app.MapPost("/login", (LoginRequest login) =>
+            {
+                if (login.User != "admin" || login.Password != "123456") return Results.Unauthorized();
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Expires = DateTime.UtcNow.AddHours(1),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(JwtSettings.Key),
+                        SecurityAlgorithms.HmacSha256Signature)
+                };
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                var tokenString = tokenHandler.WriteToken(token);
+
+                return Results.Ok(new LoginResponse(tokenString));
+            })
+            .WithName("Login")
+            .WithTags("Segurança")
+            .Produces<LoginResponse>()
+            .Produces(StatusCodes.Status401Unauthorized);
+    }
+}
