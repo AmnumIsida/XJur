caf7421fd0b0b5f4b8d3cf3e7332caae415d0783
AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>
2025-05-23 05:04:38 -0300

    add class EmitirNotaFiscalResponse
    Signed-off-by: AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>

---
+++ XAlgiz/Dtos/EmitirNotaFiscalResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace XAlgiz.Dtos;
+
+public record EmitirNotaFiscalResponse(
+    int Numero,
+    DateOnly DataDeEmissao,
+    string ClienteNome,
+    string ClienteCpfCnpj,
+    string ClienteEmail,
+    string ClienteTelefone,
+    string ServicoDescricao,
+    decimal ServicoValor,
+    DateOnly ServicoData);
