67ec6025f1bd446bdfa7d1f519edd33a31a467d0
AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>
2025-05-23 05:18:18 -0300

    add class EmitirNotaFiscalRequestValidator
    Signed-off-by: AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>

---
+++ XAlgiz/Validators/EmitirNotaFiscalRequestValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using XAlgiz.Dtos;
+
+namespace XAlgiz.Validators;
+
+public class EmitirNotaFiscalRequestValidator : AbstractValidator<EmitirNotaFiscalRequest>
+{
+    public EmitirNotaFiscalRequestValidator()
+    {
+        RuleFor(r => r.ServicoId)
+            .GreaterThan(0).WithMessage("Id do serviço inválido.");
+    }
+}
