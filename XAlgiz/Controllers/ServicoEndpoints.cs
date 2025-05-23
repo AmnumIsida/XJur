44bf89c04702600fca8dbefa480f0e9dbcbcd39b
AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>
2025-05-23 05:18:50 -0300

    add class ServicoEndpoints
    Signed-off-by: AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>

---
+++ XAlgiz/Controllers/ServicoEndpoints.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+using XAlgiz.Dtos;
+using XAlgiz.Exceptions;
+using XAlgiz.Services;
+
+namespace XAlgiz.Controllers;
+
+public static class ServicoEndpoints
+{
+    public static void MapServicoEndpoints(this WebApplication app)
+    {
+        app.MapPost("/servicos",
+                (CadastrarServicoRequest request, ServicoService service,
+                    IValidator<CadastrarServicoRequest> validator) =>
+                {
+                    var validation = validator.Validate(request);
+                    if (!validation.IsValid)
+                        return Results.BadRequest(validation.Errors.Select(e => e.ErrorMessage));
+
+                    try
+                    {
+                        return Results.Ok(service.CadastrarServico(request));
+                    }
+                    catch (ClienteNaoEncontradoException)
+                    {
+                        return Results.BadRequest(new[] { "Cliente não encontrado." });
+                    }
+                    catch
+                    {
+                        return Results.StatusCode(500);
+                    }
+                })
+            .RequireAuthorization()
+            .WithName("CadastrarServico")
+            .WithTags("Serviços")
+            .Produces<CadastrarServicoResponse>()
+            .Produces<IEnumerable<string>>(StatusCodes.Status400BadRequest)
+            .Produces(StatusCodes.Status500InternalServerError);
+    }
+}
