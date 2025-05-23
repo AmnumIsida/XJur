40c73721381470f00474480bde95f3083894923e
AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>
2025-05-23 05:14:38 -0300

    add class SecureRandomBytes
    Signed-off-by: AmnumIsida <178476581+AmnumIsida@users.noreply.github.com>

---
+++ XAlgiz/Security/SecureRandomBytes.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography;
+
+namespace XAlgiz.Security;
+
+public static class SecureRandomBytes
+{
+    public static byte[] TokenBytes(int n)
+    {
+        var bytes = new byte[n];
+        RandomNumberGenerator.Fill(bytes);
+        return bytes;
+    }
+}
